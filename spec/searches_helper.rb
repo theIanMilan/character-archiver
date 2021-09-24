d3d8506ede81387ee0b0381b5a5e5a4e0e1871aa
theIanMilan <theIanMilan@users.noreply.github.com>
2021-09-24 21:50:41 +0800

    Edit I18n of Search #create and #update

--- spec/searches_helper.rb
+++ spec/searches_helper.rb
@@ -3,13 +3,13 @@ module SearchesHelper
     visit edit_character_searches_path(character)
     page.check('Searching for connections')
     fill_in 'Search message', with: message
-    click_on 'Create Search'
+    click_on 'Create Search Details'
   end
 
   def update_character_search(character, message)
     visit edit_character_searches_path(character)
     page.check('Searching for connections')
     fill_in 'Search message', with: message
-    click_on 'Update Search'
+    click_on 'Update Search Details'
   end
 end
