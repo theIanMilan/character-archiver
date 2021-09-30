f7714bbbc86e47bf363f57b68cc56cf0c4a96fd3
theIanMilan <theIanMilan@users.noreply.github.com>
2021-09-30 19:46:12 +0800

    Add basic card design

--- app/javascript/packs/scroll-top.js
+++ app/javascript/packs/scroll-top.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
 });
 
 function scrollToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  $('html, body').stop().animate({ scrollTop: 0 }, 500);
+  return false;
 }
 
 function toggleScrollTop() {
