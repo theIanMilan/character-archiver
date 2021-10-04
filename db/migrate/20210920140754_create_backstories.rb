4885dfbb736a6d82f59598da787f6e91fdac2ee1
theIanMilan <theIanMilan@users.noreply.github.com>
2021-10-05 02:00:57 +0800

    Add additional Backstory attributes

--- db/migrate/20210920140754_create_backstories.rb
+++ db/migrate/20210920140754_create_backstories.rb
@@ -2,11 +2,15 @@ class CreateBackstories < ActiveRecord::Migration[6.1]
   def change
     create_table :backstories do |t|
       t.belongs_to :character
-      t.text :body
+      t.text :intro
+      t.text :quote_body
+      t.string :quote_author
+      t.text :appearance
       t.text :personality
       t.text :ideals
       t.text :bonds
       t.text :flaws
+      t.text :biography
       t.timestamps
     end
   end
--- test/factories/backstories.rb
+++ test/factories/backstories.rb
@@ -1,10 +1,14 @@
 FactoryBot.define do
   factory :backstory do
     association :character
-    body { Faker::Lorem.paragraphs(number: 3, supplemental: true) }
+    intro { Faker::TvShows::SiliconValley.quote }
+    quote_body { Faker::Quote.famous_last_words }
+    quote_author { Faker::TvShows::Simpsons.character }
+    appearance { Faker::TvShows::Community.quotes }
     personality { Faker::Lorem.paragraphs(number: 1, supplemental: true) }
     ideals { Faker::Lorem.paragraphs(number: 1, supplemental: true) }
     bonds { Faker::Lorem.paragraphs(number: 1, supplemental: true) }
     flaws { Faker::Lorem.paragraphs(number: 1, supplemental: true) }
+    biography { Faker::Lorem.paragraphs(number: 3, supplemental: true) }
   end
 end
