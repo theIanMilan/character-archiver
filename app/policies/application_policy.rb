1dc02036092b495ced0c68700a52854a6ef70b66
theIanMilan <theIanMilan@users.noreply.github.com>
2021-10-17 20:31:58 +0800

    Add Character Policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -4,8 +4,6 @@ class ApplicationPolicy
   attr_reader :user, :record
 
   def initialize(user, record)
-    raise Pundit::NotAuthorizedError, 'must be logged in' unless user
-
     @user = user
     @record = record
   end
