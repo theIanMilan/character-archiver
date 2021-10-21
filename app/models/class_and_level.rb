2dc07cd0c8dcb0b32eee53d5e885e55d5c3f850a
theIanMilan <theIanMilan@users.noreply.github.com>
2021-10-21 11:55:21 +0800

    Move Character#classes_string and #total_levels instance methods to ClassAndLevel as class methods

--- app/models/class_and_level.rb
+++ app/models/class_and_level.rb
@@ -15,6 +15,20 @@ class ClassAndLevel < ApplicationRecord
                                   numericality: { greater_than: 0, less_than_or_equal_to: 30 }
   validate :total_level_less_than30
 
+  def self.total_levels
+    calculate(:sum, :character_level)
+  end
+
+  def self.classes_string
+    # Returns string representing all the Character's
+    # classes, subclasses and levels
+    # e.g. Abjuration Wizard 4 and Champion Fighter 2
+
+    arr = pluck(:character_subclass, :character_class, :character_level)
+    arr.map! { |e| e.join(' ') }
+    arr.to_sentence
+  end
+
   def total_level_less_than30
     return if character_id.nil?
     return if Character.find(character_id).class_and_levels.count <= 1
