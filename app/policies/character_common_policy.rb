89c7f22fcbd66a6bfc99bdbcaecdc8240251d075
theIanMilan <theIanMilan@users.noreply.github.com>
2021-10-24 13:23:52 +0800

    Fix edit buttons not showing in Characters#show

--- app/policies/character_common_policy.rb
+++ app/policies/character_common_policy.rb
@@ -1,4 +1,8 @@
 class CharacterCommonPolicy < ApplicationPolicy
+  # In views, policy checking fails when User has no existing data for the record
+  # Opt instead to check Character policy in Views
+  # When accessing through routes, policy works as intended.
+
   def update?
     user.nil? ? false : (user.admin? || record.character.user == user)
   end
