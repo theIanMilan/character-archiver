6b092cbeba80e878066722e683f21ab031229ce0
theIanMilan <theIanMilan@users.noreply.github.com>
2021-10-25 22:47:53 +0800

    Fix global jQuery to work with Boostrap plugins and other libraries such as select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,19 +7,25 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery') // may be redundant. Also made available in environment.js as a global plugin
-window.$ = $
-require('select2')
-$.fn.select2.defaults.set("theme", "bootstrap");
+
 require("@nathanvda/cocoon")
 const flatpickr = require("flatpickr")
+
+require("bootstrap/dist/js/bootstrap.min.js")
+var jQuery = require('jquery')
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+require('select2')
+$.fn.select2.defaults.set("theme", "bootstrap");
 
 // Toastr
 global.toastr = require('toastr')
 
 // Stylesheets import
-import 'bootstrap/dist/js/bootstrap'
-import "bootstrap/dist/css/bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 import 'select2/dist/css/select2.css'
 import 'select2-bootstrap-theme/dist/select2-bootstrap'
 import "@fortawesome/fontawesome-free/css/all"
